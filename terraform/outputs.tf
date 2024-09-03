1498d979adfa9406c86e337544e7ce153a71c7d9
Arun Chaudhary <133113704+arun-kchaudhary@users.noreply.github.com>
2024-09-03 14:51:05 +0545

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,6 @@
 output "ecr_repo_url" {
-  value = aws_ecr_repository.flask_app.repository_url
+  value = aws_ecr_repository.flask_app[0].repository_url
 }
-
 output "eks_cluster_name" {
   value = aws_eks_cluster.flask_eks.name
 }
